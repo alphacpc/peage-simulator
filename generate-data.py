b0101fdfe75cbb096c5262952075d6b4236dd7cd
alphacpc <alphacpc@gmail.com>
2022-09-19 12:19:21 +0000

    Bug to connect on kafka

--- generate-data.py
+++ generate-data.py
@@ -1,18 +1,32 @@
+from ensurepip import bootstrap
 import random
 import time
 from datetime import datetime
+from kafka3 import KafkaProducer
 
 tabCars = ["C1", "C2", "C3", "C4"]
 tabCheckPoints = ["Techno pole", "Poste Thiaroye", "Keur Massar", "Rufisque", "Diamniadio"]
 tabPayment = ["Rapido", "Caisse", "Caisse"]
 
-localtime = time.asctime(time.localtime(time.time()))
+now = datetime.now()
 today = datetime.today().strftime('%Y/%m/%d')
+current_time = now.strftime("%H:%M:%S")
 
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 
 while True:
 
-    print(random.choice(tabCars), random.choice(tabCheckPoints), random.choice(tabPayment), today, localtime )
+    # print(random.choice(tabCars), random.choice(tabCheckPoints), random.choice(tabPayment), today, localtime )
+    car = random.choice(tabCars)
+    point = random.choice(tabCheckPoints)
+    payment = random.choice(tabPayment)
+
+    msg = f"{car},{point},{payment},{today},{current_time}"
+
+    print(msg)
+
+    producer.send("test-peage", bytes(msg, encoding='utf8'))
 
     time.sleep(0.1)
